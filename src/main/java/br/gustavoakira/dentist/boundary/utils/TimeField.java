7a12714093f3b535a42fc96245aaca8fe03fd790
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2021-11-22 22:00:39 -0300

    refactoring: time input refine

--- src/main/java/br/gustavoakira/dentist/boundary/utils/TimeField.java
+++ src/main/java/br/gustavoakira/dentist/boundary/utils/TimeField.java
@@ -1,13 +1,23 @@
 package br.gustavoakira.dentist.boundary.utils;
 
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.DateTimeStringConverter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class TimeField extends TextField {
+
+    public TimeField() {
+        super();
+        setText("00:00:00");
+    }
+
     @Override
     public void replaceText(int start, int end, String text) {
         if (!text.matches("[a-z]")) {
             super.replaceText(start, end, text);
         }
     }
-
 }
